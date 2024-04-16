21cdd0e1b9c85ce77d8153b6c2547aa415620971
ssharma533 <145627225+ssharma533@users.noreply.github.com>
2024-04-15 23:22:31 -0400

    Update preprocess-iot-monitor-customdata.py

--- iotsolution-scripts-data/preprocess-iot-monitor-customdata.py
+++ iotsolution-scripts-data/preprocess-iot-monitor-customdata.py
@@ -170,7 +170,7 @@ latlong=lat:long'
      identifier = "IoT device performance and failures"
 
      # if dataage - use:dataage_utcoffset_timetype
-     preprocesslogic='anomprob,trend,avg'
+     preprocesslogic='min,max,count,variance,outliers,anomprob'
 
      pathtotmlattrs='oem=n/a,lat=n/a,long=n/a,location=n/a,identifier=n/a'          
      try:
@@ -202,7 +202,7 @@ async def startviper():
           except Exception as e:
             print("ERROR:",e)
             continue
-preprocesslogic='MIN,MAX,COUNT,VARIANCE,OUTLIERS,ANOMPROB'
+
 
 async def spawnvipers():
 
